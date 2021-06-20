5f14579bfb2e1ab778877d694f7bab3a2d891bc0
frosv <616342684@qq.com>
2021-06-21 00:54:41 +0800

    feat: 将代码从简单的打印转换到生成html 调整配置文件

--- check-images-size.js
+++ check-images-size.js
@@ -1,8 +1,16 @@
 // ./remove-comments-plugin.js
 
+const colors = require('colors')
+
 const config = require('./size.config')
 
-console.log(config.name);
+const fs = require("fs");
+
+// 判断配置是否正确
+if(config.fileType.length === 0){
+  console.log('请设置需要检测类型(fileType)！'.red)
+  return
+}
 
 class CheckImagesSize {
   apply(compiler) {
@@ -10,34 +18,48 @@ class CheckImagesSize {
     compiler.hooks.emit.tap('CheckImagesSize', compilation => {
       console.log('plugins');
       console.log('\n');
-      let imgList = []
-      let jsList = []
+      let tmp = ``
       // compilation => 可以理解为此次打包的上下文
       for (const name in compilation.assets) {
 
         // 获取大小 转换成KB
         const size = ((compilation.assets[name].size()) / 1024).toFixed(3)
-        // 获取图片格式
-        if (name.endsWith('.svg') || name.endsWith('.png') || name.endsWith('.jpg')) {
 
-
-          imgList.push({
-            name,
-            size: size + 'KB'
-          })
-
-        } else if(name.endsWith('.js')){
-          jsList.push({
-            name,
-            size: size + 'KB'
-          })
-        }
+        // 根据用户设置将后缀名相同的取出
+        config.fileType.forEach(element => {
+          // 防止用户输入大写
+          if(name.endsWith(element.toLocaleLowerCase())){
+            tmp += `<tr>
+                      <td>${name}</td>
+                      <td>${size}KB</td>
+                    </tr>`
+          }
+        });
 
       }
-      console.log('png || jpg || svg 文件大小');
-      console.table(imgList);
-      console.log('js 文件大小');
-      console.table(jsList);
+
+      console.log(tmp)
+      const html = `<html>
+        <title>前端项目健康报告</title>
+        <body>
+          <div style="color: red;">目前设置单位件最大${config.maxSize}KB</div>
+          <table border="0">
+          <tr>
+            <th>Name</th>
+            <th>Size</th>
+          </tr>
+          ${tmp}
+        </table>
+        </body>
+      </html>`
+
+      fs.writeFile('前端项目健康报告.html', html, 'utf8', function (error) {
+        if (error) {
+          console.log(error);
+          return false;
+        }
+        console.log('已经生成分析报告.html');
+      })
 
       console.log('\n');
 
--- size.config.js
+++ size.config.js
@@ -1,3 +1,10 @@
+/**
+ * 配置
+ * 
+ */
 module.exports = {
-  name: 'test'
+  maxSize: 200, // 文件最大值
+  fileType: ['svg','png','jpg','js','css'], // 允许用户以数组方式写入文件类型
+  url: '', // 指定打包后文件路径
+  
 }
